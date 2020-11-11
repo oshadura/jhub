2581f12183ae62000d8d003d0ffbbdce27a9dcfe
Carl Lundstedt <root@cmsaf.unl.edu>
2020-11-11 11:00:35 -0600

    moved XCACHE_HOST to solely reside in config.yaml

--- charts/coffea-casa-jhub/files/hub/jupyterhub_config.py
+++ charts/coffea-casa-jhub/files/hub/jupyterhub_config.py
@@ -261,7 +261,7 @@ c.KubeSpawner.volume_mounts.extend(get_config('singleuser.storage.extraVolumeMou
 
 # Detect if there are tokens for this user - if so, add them as volume mounts.
 c.KubeSpawner.environment["BEARER_TOKEN_FILE"] = "/etc/cmsaf-secrets/xcache_token"
-c.KubeSpawner.environment["XCACHE_HOST"] = "red-xcache1.unl.edu"
+#c.KubeSpawner.environment["XCACHE_HOST"] = "red-xcache1.unl.edu"
 c.KubeSpawner.environment["XRD_PLUGINCONFDIR"] = "/opt/conda/etc/xrootd/client.plugins.d/"
 c.KubeSpawner.environment["LD_LIBRARY_PATH"] = "/opt/conda/lib/"
 c.KubeSpawner.volume_mounts.extend([{"name": "cmsaf-secrets", "mountPath": "/etc/cmsaf-secrets"}])
